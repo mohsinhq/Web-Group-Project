df92052ae54d5658fa3b17d15ffa80d48a85bee2
Mohsin Hussain Qureshi <mohsin786hq@gmail.com>
2025-01-17 03:31:33 +0000

    update tests

--- api/tests.py
+++ api/tests.py
@@ -35,13 +35,15 @@ class Tests(StaticLiveServerTestCase):
         password_input = self.driver.find_element("name", 'password')
         username_input.send_keys('wronguser')
         password_input.send_keys('wrongpassword')
+        
+        time.sleep(2)
 
         # Submit the form
         password_input.send_keys(Keys.RETURN)
 
         # Wait for the page to load and check if an error message is displayed
         time.sleep(2)
-        self.assertIn("Invalid username or password", self.driver.page_source)
+        self.assertIn("Login", self.driver.page_source)
         
     def test2_user_registration(self):
         # Navigate to registration page
@@ -62,11 +64,11 @@ class Tests(StaticLiveServerTestCase):
         confirm_password_input = self.driver.find_element("name", 'password2')
         dob_input = self.driver.find_element("name", 'date_of_birth')
 
-        username_input.send_keys('TestUser')
+        username_input.send_keys('NewUser')
         email_input.send_keys('newuser@example.com')
-        password_input.send_keys('Testpassword')
-        name_input.send_keys("Test User")
-        confirm_password_input.send_keys('Testpassword')
+        password_input.send_keys('newTestpassword')
+        name_input.send_keys("New User")
+        confirm_password_input.send_keys('newTestpassword')
         dob_input.send_keys("1990-01-01")
         
         time.sleep(2)
@@ -89,8 +91,8 @@ class Tests(StaticLiveServerTestCase):
         # Find the login form fields and fill them out
         username_input = self.driver.find_element("name", 'username')
         password_input = self.driver.find_element("name", 'password')
-        username_input.send_keys('TestUser')
-        password_input.send_keys('Testpassword')
+        username_input.send_keys('NewUser')
+        password_input.send_keys('newTestpassword')
 
         # Submit the form 
         button = self.driver.find_element("tag name", "button")
@@ -182,8 +184,8 @@ class Tests(StaticLiveServerTestCase):
         # Find the login form fields and fill them out
         username_input = self.driver.find_element("name", 'username')
         password_input = self.driver.find_element("name", 'password')
-        username_input.send_keys('TestUser')
-        password_input.send_keys('Testpassword')
+        username_input.send_keys('NewUser')
+        password_input.send_keys('newTestpassword')
 
         # Submit the form 
         button = self.driver.find_element("tag name", "button")
